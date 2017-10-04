ef15c7d5fd390c5a4e110153a275b73082c4a010
Kenta Sato (佐藤 建太) <bicycle1885@gmail.com>
2017-10-04 10:50:55 +0900

    accept keyword arguments of TranscodingStream (#6)

--- src/CodecZlib.jl
+++ src/CodecZlib.jl
@@ -29,6 +29,16 @@ import TranscodingStreams:
     initialize,
     finalize
 
+# TODO: This method will be added in the next version of TranscodingStreams.jl.
+function splitkwargs(kwargs, keys)
+    hits = []
+    others = []
+    for kwarg in kwargs
+        push!(kwarg[1] ∈ keys ? hits : others, kwarg)
+    end
+    return hits, others
+end
+
 include("libz.jl")
 include("compression.jl")
 include("decompression.jl")
