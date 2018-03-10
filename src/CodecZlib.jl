d7784b4155570d037b1644566f509ecd9b9fedfb
Kenta Sato <bicycle1885@gmail.com>
2018-03-10 23:02:04 +0900

    require TranscodingStreams.jl 0.5

--- src/CodecZlib.jl
+++ src/CodecZlib.jl
@@ -27,22 +27,10 @@ import TranscodingStreams:
     Memory,
     Error,
     initialize,
-    finalize
+    finalize,
+    splitkwargs
 
-# TODO: This method will be added in the next version of TranscodingStreams.jl.
-function splitkwargs(kwargs, keys)
-    hits = []
-    others = []
-    for kwarg in kwargs
-        push!(kwarg[1] ∈ keys ? hits : others, kwarg)
-    end
-    return hits, others
-end
-
-# For compatibility.
-if !isdefined(Base, :Cvoid)
-    const Cvoid = Void
-end
+using Compat: Cvoid
 
 include("libz.jl")
 include("compression.jl")
