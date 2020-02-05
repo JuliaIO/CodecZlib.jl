49ead3c536db74f0cc6ee1e55d5590af12fcb2f4
Elliot Saba <staticfloat@gmail.com>
2020-02-05 14:05:39 -0800

    Upgrade to Julia 1.3+, use artifacts
    This makes use of the new Artifacts system within Julia 1.3+.  This
    doesn't really change any core functionality, but it will benefit from
    the installation improvements we are making to artifacts and binary
    packages in general.

--- src/CodecZlib.jl
+++ src/CodecZlib.jl
@@ -27,14 +27,7 @@ import TranscodingStreams:
     initialize,
     finalize,
     splitkwargs
-using Libdl
-
-const libzpath = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
-if !isfile(libzpath)
-    error("CodecZlib.jl is not installed properly, run Pkg.build(\"CodecZlib\") and restart Julia.")
-end
-include(libzpath)
-check_deps()
+using Zlib_jll
 
 include("libz.jl")
 include("compression.jl")
