bf4b87d1420e254e19101f31a036e7456283b463
Stefan Karpinski <stefan@karpinski.org>
2020-02-07 13:50:56 -0500

    use Zlib_jll instead of deps/build.jl

--- src/CodecZlib.jl
+++ src/CodecZlib.jl
@@ -27,14 +27,9 @@ import TranscodingStreams:
     initialize,
     finalize,
     splitkwargs
-using Libdl
 
-const libzpath = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
-if !isfile(libzpath)
-    error("CodecZlib.jl is not installed properly, run Pkg.build(\"CodecZlib\") and restart Julia.")
-end
-include(libzpath)
-check_deps()
+import Zlib_jll:
+    libz
 
 include("libz.jl")
 include("compression.jl")
